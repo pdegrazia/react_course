cb435cb5618dacfe417f478eb25117d7dd664c23
paolo <paolo.de-grazia@lloydsbanking.com>
2019-12-10 23:44:38 +0000

    ex2: create a component with h1 with your name, a paragraph, a list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MyComponent() {
+function MyInfo() {
   return (
-    <ul>
-      <li>item 1</li>
-      <li>item 2</li>
-      <li>item 3</li>
-    </ul>
+    <div>
+      <h1>Paolo</h1>
+      <p>Nothing to say here</p>
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </ul>
+    </div>
   );
 }
 
-ReactDOM.render(<MyComponent />, document.getElementById('root'));
+ReactDOM.render(
+  <MyInfo/>, 
+  document.getElementById('root')
+);
 
