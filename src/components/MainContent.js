e849b1b200707317a8addf99b1bbc2f866c8b692
paolo <paolo.de-grazia@lloydsbanking.com>
2019-12-11 23:35:27 +0000

    added components for the App parent component

---
+++ src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainContent() {
+  return(
+    <div id="mainContent">This is the main content</div>
+  );
+}
+
+export default MainContent
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>This is the nav bar</nav>
+  );
+}
+
+export default Navbar
