f67b300977105d5f69809a7ba977a681bb6ba81d
paolo <paolo.de-grazia@lloydsbanking.com>
2019-12-11 23:08:59 +0000

    moved component to its own file; importing component in index.js

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>This is a paragraph</p>
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MyInfo() {
-  return (
-    <div>
-      <h1>Paolo</h1>
-      <p>Nothing to say here</p>
-      <ul>
-        <li>item 1</li>
-        <li>item 2</li>
-        <li>item 3</li>
-      </ul>
-    </div>
-  );
-}
+import MyInfo from './components/MyInfo'
 
 ReactDOM.render(
   <MyInfo/>, 
