8709eff6e955a4481662dda4e17f32748c5603bf
paolo <paolo.de-grazia@lloydsbanking.com>
2019-12-17 15:56:53 +0000

    from javascript to jsx

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 function MainContent() {
+  const firstName = "Cristiano"
+  const surname = "Ronaldo";
   return(
-    <div id="mainContent">This is the main content</div>
+    <div>
+      <div id="mainContent">This is the main content</div>
+      <p>{firstName}</p>
+      <p>{surname}</p>
+    </div>
   );
 }
 
